74351934d26b2b3476292126d4a16e39c73136cf
Valentin Fedulov <vasnake@gmail.com>
2024-05-21 12:53:24 +0300

    WIP: week9 homework, added storage

--- sandbox/week09_homework/rwa/realworld.go
+++ sandbox/week09_homework/rwa/realworld.go
@@ -3,29 +3,30 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"slices"
 	"time"
 )
 
 // сюда писать код
 
 func GetApp() http.Handler {
-	return NewConduitAppHttpHandlers()
+	return NewConduitAppHttpHandlers(NewRAMStorage())
 }
 
 type ConduitAppHttpHandlers struct {
 	storage Storage
 }
 
-func NewConduitAppHttpHandlers() *ConduitAppHttpHandlers {
+func NewConduitAppHttpHandlers(stor Storage) *ConduitAppHttpHandlers {
 	return &ConduitAppHttpHandlers{
-		storage: nil,
+		storage: stor,
 	}
 }
 
 func (srv *ConduitAppHttpHandlers) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	show("ConduitAppHttpHandlers.ServeHTTP, req: ", r.URL, r)
-	// http.Error(w, "foo", http.StatusCreated)
 	switch r.Method {
 
 	case "POST":
@@ -91,65 +92,86 @@ func (srv *ConduitAppHttpHandlers) showCurrentUser(w http.ResponseWriter, r *htt
 	panicOnError("writeResponse failed", err)
 }
 
+// updateCurrentUser: update current user profile, use storage to save data between requests
 func (srv *ConduitAppHttpHandlers) updateCurrentUser(w http.ResponseWriter, r *http.Request) {
-	// TODO: update current user profile, use storage to save data between requests
-	/*
+	var currentUser = UserProfile{
 		Email:     "golang@example.com",
+		CreatedAt: now_RFC3339(),
+		UpdatedAt: now_RFC3339(),
 		Username:  "golang",
-		Bio:"",
-		=>
-		Email:"u_golang@example.com",
-		Username:"golang",
-		Bio:"Info about golang",
-	*/
-
-	var x = &struct {
-		User UserProfile
-	}{
-		User: UserProfile{
-			// Email:     "u_golang@example.com",
-			CreatedAt: now_RFC3339(),
-			UpdatedAt: now_RFC3339(),
-			Username:  "golang",
-			// Bio:       "Info about golang",
-		},
 	}
+	var err error
 
-	err := writeResponse(w, x)
-	panicOnError("writeResponse failed", err)
-}
+	bodyMap, err := unmarshalBody(r)
+	panicOnError("unmarshalBody failed", err)
 
-func (srv *ConduitAppHttpHandlers) registerNewUser(w http.ResponseWriter, r *http.Request) {
-	var x = &struct {
-		User UserProfile
-	}{
-		User: UserProfile{
-			Email:     "golang@example.com",
-			CreatedAt: now_RFC3339(),
-			UpdatedAt: now_RFC3339(),
-			Username:  "golang",
-		},
+	userData, userExists := bodyMap["user"]
+	if userExists {
+		userMap, err := giveMeStrings(userData.(map[string]any)) // recover from panic
+		panicOnError("giveMeStrings failed", err)
+
+		user, err := getUserByEmail(srv.storage, currentUser.Email)
+		panicOnError("getUserByEmail failed", err)
+
+		user.updateFromMap(userMap)
+
+		err = putUserByEmail(srv.storage, user)
+		panicOnError("putUserByEmail failed", err)
+
+		err = writeUserResponse(w, user)
+		panicOnError("writeUserResponse failed", err)
+
+	} else {
+		show("updateCurrentUser, no user in given data")
+		http.Error(w, "oops", http.StatusBadRequest)
 	}
-
-	err := writeResponseWithCode(w, x, http.StatusCreated)
-	panicOnError("writeResponse failed", err)
 }
 
 func (srv *ConduitAppHttpHandlers) loginUser(w http.ResponseWriter, r *http.Request) {
-	show("loginUser, (email, password): ", r.FormValue("email"), r.FormValue("password"))
-	var x = &struct {
-		User UserProfile
-	}{
-		User: UserProfile{
-			Email:     "golang@example.com",
-			CreatedAt: now_RFC3339(),
-			UpdatedAt: now_RFC3339(),
-			Username:  "golang",
-		},
+	// show("loginUser, (email, password): ", r.FormValue("email"), r.FormValue("password"))
+	var user = UserProfile{
+		Email:     "golang@example.com",
+		CreatedAt: now_RFC3339(),
+		UpdatedAt: now_RFC3339(),
+		Username:  "golang",
 	}
 
-	err := writeResponse(w, x)
-	panicOnError("writeResponse failed", err)
+	// err := putUserByEmail(srv.storage, user)
+	// panicOnError("putUserByEmail failed", err)
+
+	err := writeUserResponse(w, user)
+	panicOnError("writeUserResponse failed", err)
+}
+
+func (srv *ConduitAppHttpHandlers) registerNewUser(w http.ResponseWriter, r *http.Request) {
+	var user = UserProfile{
+		Email:     "golang@example.com",
+		CreatedAt: now_RFC3339(),
+		UpdatedAt: now_RFC3339(),
+		Username:  "golang",
+	}
+
+	err := putUserByEmail(srv.storage, user)
+	panicOnError("putUserByEmail failed", err)
+
+	var u = &struct{ User UserProfile }{User: user}
+	err = writeResponseWithCode(w, u, http.StatusCreated)
+	panicOnError("writeResponseWithCode failed", err)
+}
+
+func unmarshalBody(r *http.Request) (map[string]any, error) {
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err == nil {
+		var bodyDecoded map[string]any
+		err = json.Unmarshal(bodyBytes, &bodyDecoded)
+		return bodyDecoded, err
+	}
+	return nil, err
+}
+
+func writeUserResponse(w http.ResponseWriter, user UserProfile) error {
+	var x = &struct{ User UserProfile }{User: user}
+	return writeResponse(w, x)
 }
 
 func writeResponse(w http.ResponseWriter, x any) error {
@@ -168,8 +190,63 @@ func writeResponseWithCode(w http.ResponseWriter, x any, code int) error {
 }
 
 type Storage interface {
-	Set(key, value string) error
-	Get(key string) (value string, err error)
+	// Set(key, value any) error
+	// Get(key string) (any, error)
+	GetAll() ([]any, error)
+	SetAll([]any) error
+}
+
+type RAMStorage struct {
+	data []any
+}
+
+var _ Storage = &RAMStorage{} // type check
+
+func NewRAMStorage() *RAMStorage {
+	return &RAMStorage{
+		data: make([]any, 0, 16),
+	}
+}
+
+// GetAll implements Storage.
+func (stor RAMStorage) GetAll() ([]any, error) {
+	return stor.data, nil
+}
+
+// SetAll implements Storage.
+func (stor *RAMStorage) SetAll(items []any) error {
+	stor.data = items
+	return nil
+}
+
+// getUserByEmail: use email as key to search in stored items
+func getUserByEmail(stor Storage, email string) (UserProfile, error) {
+	items, err := stor.GetAll()
+	if err == nil {
+		for _, x := range items {
+			user := x.(UserProfile) // panic if not only users in storage
+			if user.Email == email {
+				return user, nil
+			}
+		}
+	} else {
+		return UserProfile{}, err
+	}
+	return UserProfile{}, fmt.Errorf("getUserByEmail failed. email %#v not found in %d records", email, len(items))
+}
+
+// putUserByEmail: use email as key in stored items
+func putUserByEmail(stor Storage, user UserProfile) error {
+	items, err := stor.GetAll()
+	if err == nil {
+		items = slices.DeleteFunc(items, func(x any) bool {
+			return (x.(UserProfile)).Email == user.Email // panic if not only users in storage
+		})
+		items = append(items, user)
+
+		return stor.SetAll(items)
+	}
+	return err // fmt.Errorf("putUserByEmail failed. email: %#v; user: %#v", email, user)
 }
 
 type UserProfile struct {
@@ -183,6 +260,35 @@ type UserProfile struct {
 	Token     string `json:"token" testdiff:"ignore"`
 	Following bool
 }
+
+func (user *UserProfile) updateFromMap(data map[string]string) *UserProfile {
+	var x string
+	var exists bool
+
+	// TODO: add other user fields
+
+	x, exists = data["email"]
+	if exists {
+		user.Email = x
+	}
+
+	x, exists = data["bio"]
+	if exists {
+		user.Bio = x
+	}
+
+	return user
+}
+
+func giveMeStrings(xs map[string]any) (map[string]string, error) {
+	var ys = make(map[string]string, 16)
+	for k, v := range xs {
+		ys[k] = v.(string) // panic
+	}
+	return ys, nil
+}
+
+// --- system-wide tools ---
 
 func panicOnError(msg string, err error) {
 	if err != nil {
