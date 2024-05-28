c49f450700060d39d2df2507dc3f0bad4b6e791e
Valentin Fedulov <vasnake@gmail.com>
2024-05-28 17:41:00 +0300

    WIP: week10 homework, try and error

--- sandbox/week10_homework/taskbot/taskbot.go
+++ sandbox/week10_homework/taskbot/taskbot.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 	"sync/atomic"
 	"time"
 
@@ -16,60 +17,97 @@ var BotToken = "_golangcourse_test"
 
 // err := startTaskBot(context.Background(), ":8081")
 func startTaskBot(ctx context.Context, portStr string) error {
-	// сюда писать код
-	/*
-		в этом месте вы стартуете бота,
-		стартуете хттп сервер который будет обслуживать этого бота
-		инициализируете ваше приложение
-		и потом будете обрабатывать входящие сообщения
-	*/
 	bot, err := tgbotapi.NewBotAPI(BotToken)
 	panicOnError("NewBotAPI failed", err)
 	_, err = bot.SetWebhook(tgbotapi.NewWebhook(WebhookURL))
 	panicOnError("SetWebhook failed", err)
-
 	updates := bot.ListenForWebhook("/")
+
+	bh := NewTGBotHandlers(ctx, bot, updates)
 
 	show("startTaskBot: program started ...")
 	show("Starting server at: ", WebhookURL)
-	go http.ListenAndServe(portStr, nil)
 
-	for update := range updates {
-		id := update.Message.Chat.ID
-		cmd := update.Message.Text
-		show("bot got a new message, (id, text): ", id, cmd)
-		bot.Send(tgbotapi.NewMessage(id, "Нет задач"))
-	}
-
-	// [case#, user: command]
-	debugNotes := `
-bot got a new message, (id, text): 256; "/new написать бота";
-taskbot_test.go:390: [case1, 256: /new написать бота] bad results:
-			Want: map[256:Задача "написать бота" создана, id=1]
-			Have: map[256:Нет задач]
-	`
-	__dummy(debugNotes)
-
-	return nil
+	go bh.ProcessMessages()
+	return http.ListenAndServe(portStr, nil)
 }
 
 type TGBotHandlers struct {
-	ctx context.Context
+	ctx     context.Context
+	bot     *tgbotapi.BotAPI
+	updates tgbotapi.UpdatesChannel
 }
 
-func NewTGBotHandlers(ctx context.Context) *TGBotHandlers {
+func NewTGBotHandlers(ctx context.Context, bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel) *TGBotHandlers {
 	return &TGBotHandlers{
-		ctx: ctx,
+		ctx:     ctx,
+		bot:     bot,
+		updates: updates,
 	}
 }
 
-// ServeHTTP implements http.Handler.
-func (serv *TGBotHandlers) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	show("ServeHTTP: ", r.URL, r.Method, r)
-	http.Error(w, "oops 1", http.StatusNotImplemented)
+func (bh *TGBotHandlers) ProcessMessages() {
+	for {
+		select {
+
+		case <-bh.ctx.Done():
+			show("ctx.Done")
+			return
+
+		case update := <-bh.updates:
+			id := update.Message.Chat.ID
+			cmd := update.Message.Text
+			show("bot got a new message, (id, text): ", id, cmd)
+			resp := bh.execCommand(id, cmd)
+			show("bot sending response: ", resp)
+			bh.bot.Send(tgbotapi.NewMessage(id, resp))
+
+			// [case#, user: command]
+			debugNotes := `
+2024-05-28T14:39:01.582Z: bot got a new message, (id, text): 256; "/new написать бота";
+2024-05-28T14:39:01.582Z: bot sending response: "Задача \"написать бота\" создана, id=1";
+	taskbot_test.go:390: [case1, 256: /new написать бота] bad results:
+				Want: map[256:Задача "написать бота" создана, id=1]
+				Have: map[]
+`
+			__dummy(debugNotes)
+		} // end select
+	}
 }
 
-var _ http.Handler = NewTGBotHandlers(nil) // type check
+func (bh *TGBotHandlers) execCommand(id int64, cmd string) string {
+	// (id, text): 256; "/tasks";
+	// "Нет задач"
+	switch {
+
+	case cmd == "/tasks":
+		return "Нет задач"
+
+	case strings.HasPrefix(cmd, "/new "):
+		return bh.addTask(id, cutPrefix(cmd, "/new "))
+
+	default:
+		show("unknown command: ", cmd)
+		return "xz"
+	}
+}
+
+func (bh *TGBotHandlers) addTask(id int64, task string) string {
+	ut := UserTask{
+		userID: id,
+		taskID: nextID_10(),
+		task:   task,
+	}
+	res := fmt.Sprintf(`Задача "%s" создана, id=%s`, ut.task, ut.taskID)
+	// show("addTask result: ", res)
+	return res
+}
+
+type UserTask struct {
+	userID int64
+	taskID string
+	task   string
+}
 
 // это заглушка чтобы импорт сохранился
 func __dummy(a any) {
@@ -80,8 +118,17 @@ func __dummy(a any) {
 
 var atomicCounter = new(atomic.Uint64)
 
-func nextID() string {
+func nextID_36() string {
 	return strconv.FormatInt(int64(atomicCounter.Add(1)), 36)
+}
+
+func nextID_10() string {
+	return strconv.FormatInt(int64(atomicCounter.Add(1)), 10)
+}
+
+func cutPrefix(s, prefix string) string {
+	res, _ := strings.CutPrefix(s, prefix)
+	return res
 }
 
 func panicOnError(msg string, err error) {
